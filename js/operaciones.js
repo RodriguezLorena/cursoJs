200131bc43dfe06d19e44313dc70dfcca8b848b1
Lorena Rodriguez <loreleyro.89@gmail.com>
2021-10-29 03:56:29 -0300

    Primer clase js: Algoritmos simples

---
+++ js/operaciones.js
@@ -0,0 +1,36 @@
+//clase Uno//
+
+let persona1 = "Lore";
+let apellido = " Rodriguez"
+
+console.log(persona1 + apellido);
+
+
+let numero1 = "40";
+let numero2 = 60;
+let numero3 = "70";
+let numero4 = 100;
+
+let suma = parseInt(numero1) + numero2;
+let sumadeVarios = parseInt(numero1) + numero2 + numero4;
+
+let resta = numero3 - numero2;
+let restadeVarios = numero3 - numero1 - numero4;
+
+let multiplicacion = numero1 * numero4;
+let multiplicaciondeVarios = numero1 * numero2 * numero3;
+
+let division = numero3 / numero4;
+let divisiondeVarios = numero1 / numero2 / numero3 / numero4;
+
+let resto = numero1 % numero3;
+
+console.log(suma);
+console.log(sumadeVarios);
+console.log(resta);
+console.log(restadeVarios);
+console.log(multiplicacion);
+console.log(multiplicaciondeVarios);
+console.log(division);
+console.log(divisiondeVarios);
+console.log(resto);
